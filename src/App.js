2ccf87ffb3df56459dd545bc295214ffaa367d8f
trivedh <trivedhnani@gmail.com>
2020-02-15 10:19:50 -0500

    updating app with collection-preview and item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import ShopPage from './pages/shop/shop.component';
 import Homepage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
-          <Homepage></Homepage>
+        <Switch>
+            <Route exact path='/' component={Homepage}></Route>
+            <Route exact path='/shop' component={ShopPage}></Route>
+        </Switch>
     </div>
   );
 }
