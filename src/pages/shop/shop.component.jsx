43e972f58b3bcc370ceba7e955d738f747445775
trivedhnani <trivedhnani@gmail.com>
2020-03-02 06:12:20 -0500

    implemented code that pulls in shop data from firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,44 +1,55 @@
-import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'; 
-import CollectionPage from '../collection/collection.component';
-import { Route } from 'react-router-dom';
-import {firestore,convertCollectionssnapShotToMap} from '../../firebase/firebase.utils';
-import {updateCollectons} from '../../redux/shop/shop.actions';
-import {connect} from 'react-redux';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-const CollectionsOverviewWithSpinner =WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner=WithSpinner(CollectionPage);
-class ShopPage extends React.Component{
-    state={
-        loading:true
-    };
-    unsubscribeFromSnapshot=null;
-    componentDidMount(){
-        const {updateCollections}=this.props;
-        const collectionRef= firestore.collection('collections');
-        this.unsubscribeFromSnapshot=collectionRef.onSnapshot(async snapshot=>
-            { 
-                const collectionsMap =convertCollectionssnapShotToMap(snapshot)
-                updateCollections(collectionsMap);
-                this.setState({loading:false});
-            });
-        
-    }
-    render(){
-        const {match}=this.props;
-        const{loading}=this.state;
-    return(
-        <div className='shop-page'>
-        <Route exact path={`${match.path}`} render={(props)=><CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} /> 
-        <Route exact path={`${match.path}/:collectionId`} render={(props)=><CollectionPageWithSpinner isLoading={loading} {...props} />}></Route>
-        </div>
-    )
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+import { Route } from "react-router-dom";
+import {
+  firestore,
+  convertCollectionssnapShotToMap
+} from "../../firebase/firebase.utils";
+import { updateCollectons } from "../../redux/shop/shop.actions";
+import { connect } from "react-redux";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionssnapShotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        ></Route>
+      </div>
+    );
     // Props of render are important because we have to make sure that we are passing match,history and loaction dow to componets
   }
-};
-const mapDispatchToProps=dispatch=>(
-    {
-        updateCollections:(collection)=>dispatch(updateCollectons(collection))
-    }
-);
-export default connect(null,mapDispatchToProps)(ShopPage);
+}
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collection => dispatch(updateCollectons(collection))
+});
+export default connect(null, mapDispatchToProps)(ShopPage);
