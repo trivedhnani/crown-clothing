d8d172adeb3736d3d2a6141d2955420d81c56e5e
trivedh <trivedhnani@gmail.com>
2020-05-13 15:17:41 -0400

    Changed port for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 if (process.env.NODE_ENV !== "production")
   dotenv.config({ path: "./config.env" });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
